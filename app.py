53895ed51f09b7df547d3f1d7f9b8da4f525fc17
Mariana200012 <l21590079@sjuanrio.tecnm.mx>
2026-03-06 12:53:26 -0600

    Feat: Read

--- app.py
+++ app.py
@@ -1,17 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
 
 #crear instancia
 app =  Flask(__name__)
 
-#Ruta raiz
-@app.route('/')
-def index():
-    return 'Hola Mundo'
+# Configuración de la base de datos PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+#Modelo de la base de datos
+class Estudiante(db.Model):
+    __tablename__ = 'estudiantes'
+    no_control = db.Column(db.String, primary_key=True)
+    nombre = db.Column(db.String)
+    ap_paterno = db.Column(db.String)
+    ap_materno = db.Column(db.String)
+    semestre = db.Column(db.Integer)
+
+    def to_dict(self):
+        return{
+            'no_control': self.no_control,
+            'nombre': self.nombre,
+            'ap_paterno': self.ap_paterno,
+            'ap_materno': self.ap_materno,
+            'semestre': self.semestre,
+        }
+# Crear las tablas si no existen
+with app.app_context():
+    db.create_all()
+
+#Ruta raiz
 @app.route('/estudiantes')
-def getEstudiantes():
-    #return 'Aqui se mostraran los estudiantes'
-    return render_template('index.html')
+def index():
+    #Trae todos los estudiantes
+    estudiantes = Estudiante.query.all()
+    #return estudiantes
+    return render_template('index.html', estudiantes = estudiantes)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
